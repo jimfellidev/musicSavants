4bad8da6d44a41c816571c6b145400cab105653c
Jim Felli <jimfellidev@gmail.com>
2019-07-23 06:32:21 -0400

    new layout

--- script.js
+++ script.js
@@ -23,29 +23,56 @@
 
 // ----------- slideshow --------- //
 
-var slideIndex = 1;
-showSlides(slideIndex);
+// var slideIndex = 1;
+// showSlides(slideIndex);
 
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
+// function plusSlides(n) {
+//   showSlides(slideIndex += n);
+// }
 
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
+// function currentSlide(n) {
+//   showSlides(slideIndex = n);
+// }
 
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1} 
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none"; 
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block"; 
-  dots[slideIndex-1].className += " active";
-}
+// function showSlides(n) {
+//   var i;
+//   var slides = document.getElementsByClassName("mySlides");
+//   var dots = document.getElementsByClassName("dot");
+//   if (n > slides.length) {slideIndex = 1} 
+//   if (n < 1) {slideIndex = slides.length}
+//   for (i = 0; i < slides.length; i++) {
+//       slides[i].style.display = "none"; 
+//   }
+//   for (i = 0; i < dots.length; i++) {
+//       dots[i].className = dots[i].className.replace(" active", "");
+//   }
+//   slides[slideIndex-1].style.display = "block"; 
+//   dots[slideIndex-1].className += " active";
+// }
+
+// carousel();
+// function carousel() {
+//   var i;
+//   var x = document.getElementsByClassName("mySlides");
+//   for (i = 0; i < x.length; i++) {
+//     x[i].style.display = "none"; 
+//   }
+//   slideIndex++;
+//   if (slideIndex > x.length) {slideIndex = 1} 
+//   x[slideIndex-1].style.display = "block"; 
+//   setTimeout(carousel, 6000); // Change image every 2 seconds
+// }
+
+
+
+// collapse form  
+document.getElementById('form__submit').addEventListener('click',function hide() {
+    var div = document.getElementById('form');
+    div.style.display = 'none';
+    var thanks = document.getElementById('thanks');
+    thanks.style.display = 'block';
+}, false);
+
+
+   
+   
