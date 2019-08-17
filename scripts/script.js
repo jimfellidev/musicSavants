ba50c932f05873c92bcc0337067be216c8603421
Jim Felli <jimfellidev@gmail.com>
2019-08-17 06:34:00 -0400

    updates McGoo

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,4 @@
-    
+
   
 /* When the user clicks on the button, 
 toggle between hiding and showing the dropdown content */
@@ -9,76 +9,47 @@ const menu = document.querySelector('.nav__dropbtn--content');
 const menuToggle = document.querySelector('.nav__dropbtn');
 const menuItem = document.querySelector('.nav__dropbtn--item');
 const hamburger = document.querySelector(".hamburger");
-const comboContain = menu.classList.contains('show') && hamburger.classList.contains('is-active');
-
-// menuToggle.addEventListener('click', function() {
-//   document.getElementById("myDropdown").classList.toggle("show");
-// });
 
 
-
-
-
-
-
-
-
-
-  
 // ON CLICK HAMBURGER MENU OPEN/CLOSE    DOESN'T CLOSE WITH OFF CLICK?
-  hamburger.addEventListener("click", function() {
-    // Toggle class "is-active"
-    hamburger.classList.toggle("is-active");
-    //  open/close menu
-    if (hamburger.classList.contains('is-active')){
-      menuId.classList.add("show");
-    } else {
-      menuId.classList.remove("show");
-    } 
-    
-  });
-
-
-  //    ------  OFF CLICK CLOSE MENU
-document.addEventListener('mouseup', function(event) {
-  if (!comboContain) {
-    menuId.classList.remove('show');
-    hamburger.classList.remove('is-active');
+hamburger.addEventListener("click", function() {
+  // Toggle class "is-active"
+  hamburger.classList.toggle("is-active");
+  //  open/close menu
+  if (hamburger.classList.contains('is-active')){
+    menuId.classList.add("show");
+  } else {
+    menuId.classList.remove("show");
   } 
-});
-
-
-
-// This doesnt do shit.
-hamburger.addEventListener('click', function(event) {
-  if (comboContain) {
-    menu.classList.remove('show');
-    hamburger.classList.remove('is-active');
-  } 
-});
-
-
-// -------SHOULD WORK TO CLOSE WITH CLICK WHILE OFF CLICK IS ACTIVE????
-// hamburger.addEventListener('click', function(){
-//   if (hamburger.classList.contains('is-active')) {
-//     menu.classList.remove('show');
-//     hamburger.classList.remove('is-active');
-//   }
-// });
-
-
-//  -------off click close alternative
-// window.addEventListener('mouseup', function(event){
-//   if (event.target != menu) {
-//     menu.classList.remove('show');
-    
-//     // hamburger.classList.toggle('is-active')
-//   }
-// });
   
+});
 
 
+function isDescendant(parent, child) {
+  var node = child.parentNode;
+  while (node != null) {
+    if (node == parent) {
+      return true;
+    }
+    node = node.parentNode;
+  }
+  return false;
+}
 
+document.addEventListener("click", function(event){
+  console.log('event.target', event.target);    
+  const nav = document.querySelector(".nav-wrapper");
+  var isInNav = isDescendant(nav, event.target);
+  console.log('isInNav', isInNav);
+  // KD - we need to check if the menu is already open, then if the user clicked inside of the nav
+  if (hamburger.classList.contains('is-active') && isInNav) {
+    console.log('true')      
+  } else {
+    console.log('false')
+    menuId.classList.remove("show");
+    hamburger.classList.remove("is-active");
+  }
+})    
 
 
 
@@ -155,3 +126,69 @@ function throttle(func, wait, options) {
 };
 
 
+
+
+// menu scroll
+// (function() {
+//   'use strict';
+
+//   var section = document.querySelectorAll(".menu-section");
+//   var sections = {};
+//   var i = 0;
+
+//   Array.prototype.forEach.call(section, function(e) {
+//     sections[e.id] = e.offsetTop;
+//   });
+
+//   window.onscroll = function() {
+//     var scrollPosition = document.documentElement.scrollTop || document.body.scrollTop;
+
+//     for (i in sections) {
+//       if (sections[i] <= scrollPosition) {
+//         document.querySelector('.menu-active').setAttribute('class', 'nav__link--item');
+//         document.querySelector('a[href*=' + i + ']').setAttribute('class', 'menu-active');
+//       }
+//     }
+//   };
+// })();
+
+
+// classList.remove('menu-active');
+
+// var section = document.querySelectorAll(".menu-section");
+//   var sections = {};
+//   var i = 0;
+
+//   Array.prototype.forEach.call(section, function(e) {
+//     sections[e.id] = e.offsetTop;
+//   });
+
+//   window.onscroll = function() {
+//     var scrollPosition = document.documentElement.scrollTop || document.body.scrollTop;
+
+//     for (i in sections) {
+//       if (sections[i] <= scrollPosition) {
+//         document.querySelector('.menu-active').classList.remove('menu-active');
+//         document.querySelector('a[href*=' + i + ']').classList.add('menu-active');
+//       }
+//     }
+//   };
+
+
+  const links = document.querySelectorAll('.nav__link--item');
+const sections = document.querySelectorAll('.menu-section');
+// const dropLink = document.querySelectorAll('.nav__dropbtn--item');
+
+function changeLinkState() {
+  let index = sections.length;
+
+  while(--index && window.scrollY + 50 < sections[index].offsetTop) {}
+  
+  links.forEach((link) => link.classList.remove('menu-active'));
+  links[index].classList.add('menu-active');
+  // dropLink.forEach((link) => link.classList.remove('menu-active'));
+  // dropLink[index].classList.add('menu-active');
+}
+
+changeLinkState();
+window.addEventListener('scroll', changeLinkState);
